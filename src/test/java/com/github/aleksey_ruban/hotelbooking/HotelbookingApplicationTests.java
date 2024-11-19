61da4ab4e4159ee8b48d044da00bcd88240e0d5a
aleksey-ruban <alekseyruban555@gmail.com>
2024-11-19 15:27:14 +0300

    initial

---
+++ src/test/java/com/github/aleksey_ruban/hotelbooking/HotelbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aleksey_ruban.hotelbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

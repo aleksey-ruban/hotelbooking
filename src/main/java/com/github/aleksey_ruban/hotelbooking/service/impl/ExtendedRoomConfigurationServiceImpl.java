96500e11f91f0b9a5b764f01f6f5b894dc441688
aleksey-ruban <alekseyruban555@gmail.com>
2024-11-29 02:30:19 +0300

    Added ExtendedRoomConfiguration Service and RoomPhoto Service

--- src/main/java/com/github/aleksey_ruban/hotelbooking/service/impl/ExtendedRoomConfigurationServiceImpl.java
+++ src/main/java/com/github/aleksey_ruban/hotelbooking/service/impl/ExtendedRoomConfigurationServiceImpl.java
@@ -1,9 +1,13 @@
 package com.github.aleksey_ruban.hotelbooking.service.impl;
 
+import com.github.aleksey_ruban.hotelbooking.entity.ExtendedRoomConfiguration;
 import com.github.aleksey_ruban.hotelbooking.repository.ExtendedRoomConfigurationRepository;
 import com.github.aleksey_ruban.hotelbooking.service.ExtendedRoomConfigurationService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ExtendedRoomConfigurationServiceImpl implements ExtendedRoomConfigurationService {
@@ -13,5 +17,21 @@ public class ExtendedRoomConfigurationServiceImpl implements ExtendedRoomConfigu
     @Autowired
     public ExtendedRoomConfigurationServiceImpl(ExtendedRoomConfigurationRepository repository) {
         this.repository = repository;
+    }
+
+    @Override
+    public ExtendedRoomConfiguration findById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("ExtendedRoomConfiguration with Id " + id + " not found"));
+    }
+
+    @Override
+    public List<ExtendedRoomConfiguration> findAllMainPage() {
+        return repository.findAllMainPage();
+    }
+
+    @Override
+    public List<ExtendedRoomConfiguration> finsAllOrderByCoast() {
+        return repository.findAllOrderByBaseCoast();
     }
 }
--- src/main/java/com/github/aleksey_ruban/hotelbooking/service/impl/RoomPhotoServiceImpl.java
+++ src/main/java/com/github/aleksey_ruban/hotelbooking/service/impl/RoomPhotoServiceImpl.java
@@ -1,9 +1,12 @@
 package com.github.aleksey_ruban.hotelbooking.service.impl;
 
+import com.github.aleksey_ruban.hotelbooking.entity.RoomPhoto;
 import com.github.aleksey_ruban.hotelbooking.repository.RoomPhotoRepository;
 import com.github.aleksey_ruban.hotelbooking.service.RoomPhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RoomPhotoServiceImpl implements RoomPhotoService {
@@ -15,5 +18,8 @@ public class RoomPhotoServiceImpl implements RoomPhotoService {
         this.repository = repository;
     }
 
-
+    @Override
+    public List<RoomPhoto> findRoomPhotoInOrder(Long extConfigId) {
+        return repository.findByConfigByOrder(extConfigId);
+    }
 }

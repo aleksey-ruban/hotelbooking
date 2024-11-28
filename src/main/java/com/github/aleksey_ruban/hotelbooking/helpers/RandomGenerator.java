e4c3c04e2c843a506c995bfd6fb66ae0e4b544a1
aleksey-ruban <alekseyruban555@gmail.com>
2024-11-28 20:35:38 +0300

    Random Generator Helper

---
+++ src/main/java/com/github/aleksey_ruban/hotelbooking/helpers/RandomGenerator.java
@@ -0,0 +1,13 @@
+package com.github.aleksey_ruban.hotelbooking.helpers;
+
+import java.util.Random;
+
+public class RandomGenerator {
+
+    public static String generateClientToken() {
+        Random random = new Random();
+        int randomCode = random.nextInt(1000000);
+        return String.format("%06d", randomCode);
+    }
+
+}

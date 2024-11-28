66110cf91b177dd6b12650f457abdb2ff2378785
aleksey-ruban <alekseyruban555@gmail.com>
2024-11-29 00:47:12 +0300

    Implemented extendedRoomConfiguration repository and roomPhoto repository

--- src/main/java/com/github/aleksey_ruban/hotelbooking/repository/ExtendedRoomConfigurationRepository.java
+++ src/main/java/com/github/aleksey_ruban/hotelbooking/repository/ExtendedRoomConfigurationRepository.java
@@ -2,8 +2,16 @@ package com.github.aleksey_ruban.hotelbooking.repository;
 
 import com.github.aleksey_ruban.hotelbooking.entity.ExtendedRoomConfiguration;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ExtendedRoomConfigurationRepository extends JpaRepository<ExtendedRoomConfiguration, Long> {
+    @Query("SELECT c FROM ExtendedRoomConfiguration c WHERE c.isOnMain=True ORDER BY c.BaseRoomConfiguration.baseCoast ASC")
+    List<ExtendedRoomConfiguration> findAllMainPage();
+
+    @Query("SELECT c FROM ExtendedRoomConfiguration c ORDER BY c.BaseRoomConfiguration.baseCoast ASC")
+    List<ExtendedRoomConfiguration> findAllOrderByBaseCoast();
 }
--- src/main/java/com/github/aleksey_ruban/hotelbooking/repository/RoomPhotoRepository.java
+++ src/main/java/com/github/aleksey_ruban/hotelbooking/repository/RoomPhotoRepository.java
@@ -2,8 +2,14 @@ package com.github.aleksey_ruban.hotelbooking.repository;
 
 import com.github.aleksey_ruban.hotelbooking.entity.RoomPhoto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RoomPhotoRepository extends JpaRepository<RoomPhoto, Long> {
+    @Query("SELECT p FROM RoomPhoto p WHERE p.extendedRoomConfiguration.id=:extConfigId ORDER BY p.outputOrder ASC")
+    List<RoomPhoto> findByConfigByOrder(@Param("extConfigId") Long extConfigId);
 }

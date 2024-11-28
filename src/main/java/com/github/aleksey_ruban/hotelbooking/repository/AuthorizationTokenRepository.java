134ac8d42fa3701178020ea3c2387c81cc866678
aleksey-ruban <alekseyruban555@gmail.com>
2024-11-28 21:04:33 +0300

    Fix finding auth token by telephone number instead of id

--- src/main/java/com/github/aleksey_ruban/hotelbooking/repository/AuthorizationTokenRepository.java
+++ src/main/java/com/github/aleksey_ruban/hotelbooking/repository/AuthorizationTokenRepository.java
@@ -2,8 +2,12 @@ package com.github.aleksey_ruban.hotelbooking.repository;
 
 import com.github.aleksey_ruban.hotelbooking.entity.AuthorizationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuthorizationTokenRepository extends JpaRepository<AuthorizationToken, Long> {
+    @Query("SELECT t FROM AuthorizationToken t WHERE t.client.phoneNumber = :clientPhoneNumber")
+    AuthorizationToken findByClientPhoneNumber(@Param("clientPhoneNumber") Long clientPhoneNumber);
 }

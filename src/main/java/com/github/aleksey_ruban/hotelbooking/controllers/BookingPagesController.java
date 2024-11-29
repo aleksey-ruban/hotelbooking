41237a199c35031e8dd7cb6c966fbc03644ff298
aleksey-ruban <alekseyruban555@gmail.com>
2024-11-29 21:56:59 +0300

    change index page template path

--- src/main/java/com/github/aleksey_ruban/hotelbooking/controllers/BookingPagesController.java
+++ src/main/java/com/github/aleksey_ruban/hotelbooking/controllers/BookingPagesController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class BookingPagesController {
     @GetMapping({"/", "home", "/home/"})
     public String index() {
-        return "index";
+        return "booking/index";
     }
 
     @GetMapping({"/rooms", "/rooms/"})

6f1c17e6b9406a910b7c7a38165a08d9d6064179
aleksey-ruban <alekseyruban555@gmail.com>
2024-12-06 15:15:47 +0300

    Added room service

--- src/main/java/com/github/aleksey_ruban/hotelbooking/service/impl/RoomServiceImpl.java
+++ src/main/java/com/github/aleksey_ruban/hotelbooking/service/impl/RoomServiceImpl.java
@@ -1,9 +1,13 @@
 package com.github.aleksey_ruban.hotelbooking.service.impl;
 
+import com.github.aleksey_ruban.hotelbooking.entity.Room;
 import com.github.aleksey_ruban.hotelbooking.repository.RoomRepository;
 import com.github.aleksey_ruban.hotelbooking.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RoomServiceImpl implements RoomService {
@@ -13,5 +17,15 @@ public class RoomServiceImpl implements RoomService {
     @Autowired
     public RoomServiceImpl(RoomRepository repository) {
         this.repository = repository;
+    }
+
+    @Override
+    public List<Room> findRoomsByGuestsCount(Integer guestsCount) {
+        return repository.findRoomsByGuestsCount(guestsCount);
+    }
+
+    @Override
+    public Optional<Room> findById(Long id) {
+        return repository.findById(id);
     }
 }
